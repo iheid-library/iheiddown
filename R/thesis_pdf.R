a6af16267ca1c672d754fec10ea538be2492ce6b
James Hollway <james.hollway@graduateinstitute.ch>
2020-06-17 15:54:58 +0200

    Added thesis_pdf() to support simpler knitting (and updates) 🧶

---
+++ R/thesis_pdf.R
@@ -0,0 +1,18 @@
+#' Creates an R Markdown PDF Thesis document
+#'
+#' This is a function called in output in the YAML of the driver Rmd file
+#' to specify using the IHEID Thesis LaTeX template file.
+#'
+#' @export
+#'
+#' @return A modified \code{pdf_document} based on the IHEID Thesis LaTeX
+#'   template
+#' @examples
+#' \dontrun{
+#' output:iheiddown::thesis_pdf
+#' }
+thesis_pdf <- function(...){
+  bookdown::pdf_book(..., 
+                     template = system.file('rmarkdown', 'templates', 'thesis_pdf', 'resources', 'template.tex', 
+                                                 package = 'iheiddown'))
+}
