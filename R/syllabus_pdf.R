792d0ff38856f9c7061bbcf5eb2ced20824a546d
James Hollway <james.hollway@graduateinstitute.ch>
2020-07-09 15:19:33 +0200

    Re #13, using syllabus_pdf() as wrapper for render() rather than pdf_document()

--- R/syllabus_pdf.R
+++ R/syllabus_pdf.R
@@ -5,14 +5,20 @@
 #'
 #' @export
 #'
+#' @param input The name of the .Rmd file to serve as input.
 #' @return A modified \code{pdf_document} based on the IHEID Syllabus LaTeX
 #'   template
 #' @examples
 #' \dontrun{
-#' output:iheiddown::syllabus_pdf
+#' output:
+#'   iheiddown::syllabus_pdf:
+#'     input: "Untitled.Rmd" # Replace with whatever the name of this file is, e.g. "MINT001.Rmd"
 #' }
 syllabus_pdf <- function(...){
-  rmarkdown::pdf_document(..., latex_engine = "xelatex",
-                     template = system.file('rmarkdown', 'templates', 'syllabus_pdf', 'resources', 'template.tex', 
-                                                 package = 'iheiddown'))
+  rmarkdown::render(input, 
+                    ..., 
+                    output_format = rmarkdown::pdf_document(..., latex_engine = "xelatex",
+                     template = system.file('rmarkdown', 'templates', 'syllabus_pdf', 'resources', 'template.tex',
+                                                 package = 'iheiddown'),
+                     ))
 }
