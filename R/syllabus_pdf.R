0ca22621f7ddc660cdf2dce9f0fa6acb9ba2be18
James Hollway <james.hollway@graduateinstitute.ch>
2020-07-09 15:20:53 +0200

    Added autonaming of syllabi, with course code and date

--- R/syllabus_pdf.R
+++ R/syllabus_pdf.R
@@ -6,16 +6,20 @@
 #' @export
 #'
 #' @param input The name of the .Rmd file to serve as input.
+#' @param output The file name for the exported PDF. Giving the course code variable '*code'
+#' specified in the YAML above provides an automatically named file.
 #' @return A modified \code{pdf_document} based on the IHEID Syllabus LaTeX
-#'   template
+#'   template, automatically named by course and date.
 #' @examples
 #' \dontrun{
 #' output:
 #'   iheiddown::syllabus_pdf:
 #'     input: "Untitled.Rmd" # Replace with whatever the name of this file is, e.g. "MINT001.Rmd"
+#'     output_file: *code
 #' }
-syllabus_pdf <- function(...){
+syllabus_pdf <- function(input, output_file, ...){
   rmarkdown::render(input, 
+                    output_file = paste0(output_file, "_Syllabus_", Sys.Date()), # = paste(rmarkdown::metadata$code,'.pdf'), 
                     ..., 
                     output_format = rmarkdown::pdf_document(..., latex_engine = "xelatex",
                      template = system.file('rmarkdown', 'templates', 'syllabus_pdf', 'resources', 'template.tex',
