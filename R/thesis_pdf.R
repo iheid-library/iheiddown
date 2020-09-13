d45ad45f401768f42871cefc9ab469dc109bbea2
James Hollway <james.hollway@graduateinstitute.ch>
2020-09-13 20:13:54 +0200

    Added chapter_pdf() for chapter previews

--- R/thesis_pdf.R
+++ R/thesis_pdf.R
@@ -19,3 +19,24 @@ thesis_pdf <- function(...){
                                                         template = system.file('rmarkdown', 'templates', 'thesis_pdf', 'resources', 'template.tex',
                                                                                package = 'iheiddown')))
 }
+
+#' Creates an R Markdown PDF Thesis chapter
+#'
+#' This function can be used in the header of each constituent chapter of an IHEID thesis
+#' to output a draft version of the chapter for proofreading or sending to your supervisor.
+#'
+#' @export
+#'
+#' @return A modified \code{pdf_document} based on the IHEID Thesis LaTeX
+#'   template
+#' @examples
+#' \dontrun{
+#' knit: iheiddown::chapter_pdf
+#' }
+chapter_pdf <- function(...){
+  bookdown::preview_chapter(...,
+                        output_file = paste0("Chapter_", Sys.Date()), 
+                        output_format = rmarkdown::pdf_document(latex_engine = "xelatex",
+                                                           template = system.file('rmarkdown', 'templates', 'chapter_pdf', 'resources', 'template.tex',
+                                                                                  package = 'iheiddown')))
+}
