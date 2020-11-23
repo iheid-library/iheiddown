93e62d596da25e48643767ee8bbe48de73303f3a
James Hollway <james.hollway@graduateinstitute.ch>
2020-11-23 14:48:58 +0100

    Added author details to output filename, exports chapters to versions/ too

--- R/thesis_pdf.R
+++ R/thesis_pdf.R
@@ -11,10 +11,10 @@
 #' \dontrun{
 #' knit: iheiddown::thesis_pdf
 #' }
-thesis_pdf <- function(...){
+thesis_pdf <- function(..., author){
   options(bookdown.render.file_scope = FALSE)
   bookdown::render_book(..., 
-                        output_file = paste0("Thesis_", Sys.Date()), 
+                        output_file = paste0(author, "_", "Thesis_", Sys.Date()), 
                         output_format = bookdown::pdf_book(latex_engine = "xelatex",
                                                         template = system.file('rmarkdown', 'templates', 'thesis_pdf', 'resources', 'template.tex',
                                                                                package = 'iheiddown')))
@@ -33,9 +33,9 @@ thesis_pdf <- function(...){
 #' \dontrun{
 #' knit: iheiddown::chapter_pdf
 #' }
-chapter_pdf <- function(...){
+chapter_pdf <- function(..., author, chapter){
   bookdown::preview_chapter(...,
-                        output_file = paste0("Chapter_", Sys.Date()), 
+                        output_file = paste0("versions/", author, "_", "Chapter_", chapter, "_", Sys.Date()), 
                         output_format = rmarkdown::pdf_document(latex_engine = "xelatex",
                                                            template = system.file('rmarkdown', 'templates', 'chapter_pdf', 'resources', 'template.tex',
                                                                                   package = 'iheiddown')))
