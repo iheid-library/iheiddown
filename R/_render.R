ad9682b9f6d565adbe29f9a1394b4d6e5fe25bb7
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-02-23 14:13:52 +0100

    Corrected _render.R script (hadn't taken over all changes in the last commit)

--- R/_render.R
+++ R/_render.R
@@ -6,11 +6,11 @@ rmarkdown::draft(file = "00-Example", template = "Thesis",
                  package = "iheiddown", create_dir = TRUE, edit = FALSE)
 setwd("00-Example") # Since we can only knit from the WD
 #Step 2 render the example project and replace in template folder
-iheiddown::thesis_pdf("00-Example.Rmd", author = "John Smith")
+iheiddown::thesis_pdf("00-Example.Rmd", author = "JohnSmith")
 #Come back to the original WD and copy the output while also renaming it
 setwd("..")
 date <- Sys.Date()
-name <- paste0("John Smith_Thesis_", date,".pdf")
+name <- paste0("JohnSmith_Thesis_", date,".pdf")
 target_path <- "00-Example/versions"
 file.rename(file.path(target_path, name), file.path(target_path, "IHEIDThesisExample.pdf"))
 # In the future: Add automatic rendering for syllabus and presentation templates
