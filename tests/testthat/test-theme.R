6913d7137423a57f90a13ef7c1a760a261e922ef
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-05-04 22:11:57 +0200

    Adapted theme_iheid() tests to new version

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -1,9 +1,12 @@
 test_that("theme_iheid() axes are correct", {
   library(ggplot2)
   p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point() + theme_iheid()
-  expect_identical(p$theme$axis.text$family, "sans")
-  expect_identical(unname(p$theme$axis.text$colour), "#5c666f")
-  expect_identical(p$theme$axis.text$size, 14)
+  expect_identical(p$theme$axis.text.x$family, "sans")
+  expect_identical(p$theme$axis.text.x$family, "sans")
+  expect_identical(unname(p$theme$axis.text.x$colour), "#5c666f")
+  expect_identical(unname(p$theme$axis.text.y$colour), "#5c666f")
+  expect_identical(p$theme$axis.text.x$size, 11.5)
+  expect_identical(p$theme$axis.text.y$size, 11.5)
 })
 
 test_that("theme_iheid() title is correct", {
@@ -11,5 +14,5 @@ test_that("theme_iheid() title is correct", {
   p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
   expect_identical(p$theme$plot.title$family, "sans")
   expect_identical(unname(p$theme$plot.title$colour), "#E20020")
-  expect_identical(p$theme$plot.title$size, 28)
+  expect_identical(p$theme$plot.title$size, 18)
 })
