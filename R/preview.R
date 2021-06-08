d1ff292b66f79ec12423e76563ed1513dd89fa71
James Hollway <james.hollway@graduateinstitute.ch>
2021-06-08 15:45:30 +0200

    Nested servr-based functions

--- R/preview.R
+++ R/preview.R
@@ -3,19 +3,24 @@
 #' Use the \pkg{servr} package to continuously serve and reload
 #' the Rmd document.
 #' @name preview
-#' @param moon The input Rmd file path (if missing and in RStudio, the current
+#' @param file_path The input Rmd file path (if missing and in RStudio, the current
 #'   active document is used).
-#' @param cast_from The root directory of the server.
+#' @param root_dir The root directory of the server.
 #' @param ... Passed to \code{rmarkdown::\link[rmarkdown]{render}()}.
 #' @references xaringan
 #' @importFrom xaringan inf_mr
 #' @export
-preview_realtime <- xaringan::inf_mr
+preview_realtime <- function(file_path, root_dir, ...){
+  xaringan::inf_mr(moon = file_path, cast_from = root_dir, ...)
+}
 
 #' @rdname preview
 #' @param which A integer vector of the server IDs; 
 #' by default, IDs of all existing servers in the current R session obtained from `daemon_list()`, 
 #' i.e., all daemon servers will be stopped by default.
-#' @importFrom servr daemon_stop
+#' @importFrom servr daemon_stop daemon_list
 #' @export
-preview_stop <- servr::daemon_stop
+preview_stop <- function(which = NULL){
+  if(is.null(which)) which <- servr::daemon_list()
+  servr::daemon_stop(which)
+}
