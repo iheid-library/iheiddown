340657f17a7ba0e7c021f432b6fba03a3bb419f7
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-06-09 17:11:21 +0200

    Added tests for theme_iheid()

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -16,3 +16,31 @@ test_that("theme_iheid() title is correct", {
   expect_identical(unname(p$theme$plot.title$colour), "#E20020")
   expect_identical(p$theme$plot.title$size, 18)
 })
+
+test_that("theme_iheid() grid is correct", {
+  library(ggplot2)
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  expect_identical(p[["theme"]][["panel.grid"]][["colour"]][["IHEIDBlack"]],
+                   "#5c666f")
+  expect_identical(p[["theme"]][["panel.grid"]][["size"]], 0.2)
+})
+
+test_that("theme_iheid() caption style is correct", {
+  library(ggplot2)
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  expect_identical(p[["theme"]][["plot.caption"]][["colour"]][["IHEIDGrey"]],
+                   "#6f7072")
+  expect_identical(p[["theme"]][["plot.caption"]][["family"]], "sans")
+  expect_identical(p[["theme"]][["plot.caption"]][["face"]], "italic")
+  expect_identical(p[["theme"]][["plot.caption"]][["size"]], 9)
+})
+
+test_that("theme_iheid() caption style is correct", {
+  library(ggplot2)
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  expect_identical(p[["theme"]][["plot.caption"]][["colour"]][["IHEIDGrey"]],
+                   "#6f7072")
+  expect_identical(p[["theme"]][["plot.caption"]][["family"]], "sans")
+  expect_identical(p[["theme"]][["plot.caption"]][["face"]], "italic")
+  expect_identical(p[["theme"]][["plot.caption"]][["size"]], 9)
+})
