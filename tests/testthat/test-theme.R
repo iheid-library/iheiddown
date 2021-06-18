d2d16b988df8971c082b2a29f9723337be5cf781
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-06-18 13:36:52 +0200

    Fixed more lintr errors

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -1,6 +1,5 @@
 test_that("theme_iheid() axes are correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p$theme$axis.text.x$family, "sans")
   expect_identical(p$theme$axis.text.x$family, "sans")
   expect_identical(unname(p$theme$axis.text.x$colour), "#5c666f")
@@ -10,24 +9,21 @@ test_that("theme_iheid() axes are correct", {
 })
 
 test_that("theme_iheid() title is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p$theme$plot.title$family, "sans")
   expect_identical(unname(p$theme$plot.title$colour), "#E20020")
   expect_identical(p$theme$plot.title$size, 18)
 })
 
 test_that("theme_iheid() grid is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p[["theme"]][["panel.grid"]][["colour"]][["IHEIDBlack"]],
                    "#5c666f")
   expect_identical(p[["theme"]][["panel.grid"]][["size"]], 0.2)
 })
 
 test_that("theme_iheid() caption style is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p[["theme"]][["plot.caption"]][["colour"]][["IHEIDGrey"]],
                    "#6f7072")
   expect_identical(p[["theme"]][["plot.caption"]][["family"]], "sans")
@@ -36,16 +32,14 @@ test_that("theme_iheid() caption style is correct", {
 })
 
 test_that("theme_iheid() strip style is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p[["theme"]][["strip.text"]][["family"]], "sans")
   expect_identical(p[["theme"]][["strip.text"]][["size"]], 12)
   expect_identical(p[["theme"]][["strip.text"]][["face"]], "plain")
 })
 
 test_that("theme_iheid() subtitle style is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p[["theme"]][["plot.subtitle"]][["family"]], "sans")
   expect_identical(p[["theme"]][["plot.subtitle"]][["size"]], 12)
   expect_identical(p[["theme"]][["plot.subtitle"]][["face"]], "plain")
@@ -53,11 +47,11 @@ test_that("theme_iheid() subtitle style is correct", {
 })
 
 test_that("theme_iheid() caption style is correct", {
-  library(ggplot2)
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + ggtitle("Test") + geom_point() + theme_iheid()
+  p <- ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) + ggplot2::ggtitle("Test") + ggplot2::geom_point() + iheiddown::theme_iheid()
   expect_identical(p[["theme"]][["plot.caption"]][["colour"]][["IHEIDGrey"]],
                    "#6f7072")
   expect_identical(p[["theme"]][["plot.caption"]][["family"]], "sans")
   expect_identical(p[["theme"]][["plot.caption"]][["face"]], "italic")
   expect_identical(p[["theme"]][["plot.caption"]][["size"]], 9)
 })
+
