6d6f02bd718cd4c4bb99a721c3eeae01c4ee7572
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-08-09 16:21:38 +0200

    Addressed lintr issues in tests

--- tests/testthat/test-countwords.R
+++ tests/testthat/test-countwords.R
@@ -1,6 +1,6 @@
 test_that("You can count on count_words()", {
   rmarkdown::draft(file = "test", template = "html_vignette",
                    package = "rmarkdown", create_dir = TRUE, edit = FALSE)
-  testthat::expect_equal( iheiddown::count_words("test/test.Rmd"), 241)
+  testthat::expect_equal(iheiddown::count_words("test/test.Rmd"), 241)
   unlink("test", recursive = TRUE)
 })
--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -1,4 +1,4 @@
 test_that("dates increment properly", {
-  expect_equal(advdate("2020-09-14",2), "(21 Sep)")
-  expect_equal(advdate("2020-09-14",4), "(5 Oct)")
+  expect_equal(advdate("2020-09-14", 2), "(21 Sep)")
+  expect_equal(advdate("2020-09-14", 4), "(5 Oct)")
 })
