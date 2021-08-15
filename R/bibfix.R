1e2009ebc89e6f59f39ff3088324724c4b7782d9
James Hollway <james.hollway@graduateinstitute.ch>
2021-08-15 19:28:29 +0200

    Added get_used_bib() for obtaining only used references

--- R/bibfix.R
+++ R/bibfix.R
@@ -1,6 +1,7 @@
 #' @export
 fix_bib <- function(bib_file){
-  bib <- suppressWarnings(bib2df::bib2df(bib_file))
+  if(missing(bib_file)) bib_file <- find_bib()
+  bib <- suppressMessages(bib2df::bib2df(bib_file))
   bib$PAGES <- stringr::str_replace_all(bib$PAGES, stringr::fixed("{\\textendash}"), "--")
   bib$AUTHOR <- purrr::map(bib$AUTHOR, function(x){
     x <- stringr::str_replace(x, "^", "{")
@@ -15,4 +16,26 @@ fix_bib <- function(bib_file){
     x <- stringr::str_replace(x, "\\}\\}", "}")
   })
   bib2df::df2bib(bib, bib_file)
+}
+
+
+#' @export
+get_used_bib <- function(bib_file, rmd_file){
+  if(missing(bib_file)) bib_file <- find_bib()
+  if(missing(rmd_file)) rmd_file <- rstudioapi::getSourceEditorContext()$path
+  bib <- suppressMessages(bib2df::bib2df(bib_file))
+  current_bibs <- readr::read_lines(rmd_file) %>%
+    tibble::as_tibble() %>%
+    remove_front_matter() %>%
+    remove_code_chunks() %>%
+    remove_inline_code() %>%
+    remove_html_comment() %>%
+    get_biblines() %>%
+    dplyr::mutate(value = stringr::str_remove(value, "@")) %>%
+    select(value) %>% c()
+  current_bibs$value
+}
+
+get_biblines <- function(x){
+  filter(x, grepl("@", .data$value))
 }
