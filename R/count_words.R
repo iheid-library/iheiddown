27c54b3d34fe086d10380faf5f65fad3af29a0de
James Hollway <james.hollway@graduateinstitute.ch>
2026-05-07 19:10:46 +0200

    Moved to more recent dplyr syntax

--- R/count_words.R
+++ R/count_words.R
@@ -63,40 +63,40 @@ is_odd <- function(x, val) {
 # Helper function to remove front matter (lines starting with "---" and
 # anything between)
 remove_front_matter <- function(x) {
-  mutate(x, is_code = cumsum(grepl("^---", .data$value))) %>%
-    group_by(.data$is_code) %>%
-    mutate(start_end = lag(.data$is_code, 1)) %>%
+  mutate(x, is_code = cumsum(grepl("^---", value))) %>%
+    group_by(is_code) %>%
+    mutate(start_end = lag(is_code, 1)) %>%
     ungroup() %>%
-    filter(!is_odd(.data$is_code), !is.na(.data$start_end)) %>%
-    select(-.data$is_code, -.data$start_end)
+    filter(!is_odd(is_code), !is.na(start_end)) %>%
+    select(-is_code, -start_end)
 }
 
 # Helper function for removing knitr code chunks (lines starting with "```"
 # and anything in between)
 remove_code_chunks <- function(x) {
-  mutate(x, is_code = cumsum(grepl("^```", .data$value))) %>%
-    group_by(.data$is_code) %>%
-    mutate(start_end = lag(.data$is_code, 1)) %>%
+  mutate(x, is_code = cumsum(grepl("^```", value))) %>%
+    group_by(is_code) %>%
+    mutate(start_end = lag(is_code, 1)) %>%
     ungroup() %>%
-    mutate(is_odd = is_odd(.data$is_code)) %>%
-    filter(.data$is_odd != TRUE, !is.na(.data$start_end)) %>%
-    select(-.data$is_code, -.data$start_end, -.data$is_odd)
+    mutate(is_odd = is_odd(is_code)) %>%
+    filter(is_odd != TRUE, !is.na(start_end)) %>%
+    select(-is_code, -start_end, -is_odd)
 }
 
 # Helper function to remove inline code (lines starting with "`r")
 remove_inline_code <- function(x) {
-  filter(x, !grepl("`r", .data$value))
+  filter(x, !grepl("`r", value))
 }
 
 # Helper function to remove HTML comments (lines starting with "<!--" and "-->")
 remove_html_comment <- function(x) {
-  mutate(x, start_comment = cumsum(grepl("^<!-- ", .data$value)),
-         end_comment = cumsum(grepl(" -->", .data$value))) %>%
-    group_by(.data$start_comment, .data$end_comment) %>%
-    mutate(start_end = lag(.data$start_comment, 1)) %>%
+  mutate(x, start_comment = cumsum(grepl("^<!-- ", value)),
+         end_comment = cumsum(grepl(" -->", value))) %>%
+    group_by(start_comment, end_comment) %>%
+    mutate(start_end = lag(start_comment, 1)) %>%
     ungroup() %>%
-    mutate(remove = if_else(.data$start_comment - .data$end_comment == 1 |
-                              is.na(.data$start_end), 1, 0)) %>%
+    mutate(remove = if_else(start_comment - end_comment == 1 |
+                              is.na(start_end), 1, 0)) %>%
     filter(remove != TRUE) %>%
-    select(-.data$start_comment, -.data$end_comment, -.data$remove)
+    select(-start_comment, -end_comment, -remove)
 }
